9220ee2872c29a35a548ae7c91553a7f073196ae
joebutler1966 <144394665+joebutler1966@users.noreply.github.com>
2026-07-07 21:03:45 -0400

    Add BXK Core template

---
+++ bxk_app/b_x_k_core.py
@@ -0,0 +1,20 @@
+"""
+BXK Trader Pro
+
+Module:
+BXKCore
+
+Version:
+V4
+
+Purpose:
+TODO
+"""
+
+
+class BXKCore:
+    def __init__(self):
+        pass
+
+    def evaluate(self):
+        return {}
