a18e5b30fa34994a1a5e439888308b0b0bf63f47
joebutler1966 <144394665+joebutler1966@users.noreply.github.com>
2026-07-16 20:16:55 -0400

    update BXK trader pro.bat

--- bxk_app/Start BXK Trader Pro.bat
+++ bxk_app/Start BXK Trader Pro.bat
@@ -1,18 +1,12 @@
+
 @echo off
 title BXK Trader Pro
 
 cd /d "%~dp0"
 
-echo.
-echo ============================
-echo    Starting BXK Trader Pro
-echo ============================
-echo.
-
 call .venv\Scripts\activate.bat
 
-git checkout v4
-git pull
+git pull origin main
 
 start "" http://127.0.0.1:8000
 
